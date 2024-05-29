53144bf20ba04b0cd078fb7e64516eb722296ba3
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-05-29 18:23:22 -0300

    feat: domain services

---
+++ src/main/java/br/com/alura/codechella/application/usuario/EnviarCupomDesconto.java
@@ -0,0 +1,8 @@
+package br.com.alura.codechella.application.usuario;
+
+import br.com.alura.codechella.infra.usuario.Usuario;
+
+public interface EnviarCupomDesconto {
+
+  void enviarPara(Usuario usuario);
+}
--- src/main/java/br/com/alura/codechella/domain/evento/Evento.java
+++ src/main/java/br/com/alura/codechella/domain/evento/Evento.java
@@ -5,15 +5,30 @@ import br.com.alura.codechella.domain.ingresso.TipoIngresso;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Evento {
 
+  private UUID uuid;
   private Categoria categoria;
   private String descricao;
   private LocalDateTime data;
   private Endereco endereco;
 
   private List<TipoIngresso> tipoIngressos = new ArrayList<>();
+
+  public Evento(Categoria categoria, String descricao, LocalDateTime data, Endereco endereco) {
+    this.categoria = categoria;
+    this.descricao = descricao;
+    this.data = data;
+    this.endereco = endereco;
+
+    createUuid();
+  }
+
+  private void createUuid() {
+    this.uuid = UUID.randomUUID();
+  }
 
   public void incluiNovoTipoDeIngresso(TipoIngresso tipoIngresso) {
     tipoIngressos.add(tipoIngresso);
